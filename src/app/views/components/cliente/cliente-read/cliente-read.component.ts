663d39ca019512d9a4232a61b2921941b56993ca
padreco27 <paulo_ferreira5@live.com>
2022-01-08 18:37:58 -0300

    finalizando o tecnico

--- src/app/views/components/cliente/cliente-read/cliente-read.component.ts
+++ src/app/views/components/cliente/cliente-read/cliente-read.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { Cliente } from 'src/app/model/cliente';
+
+import { ClienteService } from 'src/app/services/cliente.service';
 
 @Component({
   selector: 'app-cliente-read',
   templateUrl: './cliente-read.component.html',
   styleUrls: ['./cliente-read.component.css']
 })
-export class ClienteReadComponent implements OnInit {
+export class ClienteReadComponent implements AfterViewInit {
 
-  constructor() { }
+  cliente: Cliente[] = [];
 
-  ngOnInit(): void {
+  displayedColumns: string[] = ['id', 'nome', 'cpf', 'telefone', 'action'];
+  dataSource = new MatTableDataSource<Cliente>(this.cliente);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(
+    private service : ClienteService,
+    private router : Router) {}
+
+  ngAfterViewInit() {
+    this.findAll();
+  }
+
+  findAll():void {
+    this.service.findAll().subscribe((resposta) => {
+      this.cliente = resposta;
+      this.dataSource = new MatTableDataSource<Cliente>(this.cliente);
+      this.dataSource.paginator = this.paginator;
+    })
+  }
+
+  navigateToCreate():void {
+    this.router.navigate(['clientes/create'])
   }
 
 }
